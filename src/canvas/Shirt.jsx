7be0738353bbf3a6aeea2c99bbb8cbdd29549c57
Ali Salihi <122288564+alisalihi@users.noreply.github.com>
2025-02-10 21:44:33 +0100

    Customizer,3D Canvas,3js Camera,3js Lighting,Color Picker,File Picker

---
+++ src/canvas/Shirt.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { easing } from 'maath';
+import { useSnapshot } from 'valtio';
+import { useFrame } from '@react-three/fiber';
+import { Decal, useGLTF, useTexture } from '@react-three/drei';
+
+import state from '../store';
+
+const Shirt = () => {
+  const snap = useSnapshot(state);
+  const { nodes, materials } = useGLTF('/shirt_baked.glb');
+
+  // Ensure textures are only used if they exist
+  const logoTexture = snap.logoDecal ? useTexture(snap.logoDecal) : null;
+  const fullTexture = snap.fullDecal ? useTexture(snap.fullDecal) : null;
+
+  useFrame((state, delta) => 
+    easing.dampC(materials.lambert1.color, snap.color, 0.25, delta)
+  );
+
+  const stateString = JSON.stringify(snap);
+
+  return (
+    <group key={stateString}>
+      <mesh
+        castShadow
+        geometry={nodes.T_Shirt_male.geometry}
+        material={materials.lambert1}
+        material-roughness={1}
+        dispose={null}
+      >
+        {snap.isFullTexture && fullTexture && (
+          <Decal 
+            position={[0, 0, 0]}
+            rotation={[0, 0, 0]}
+            scale={1}
+            map={fullTexture}
+          />
+        )}
+
+        {snap.isLogoTexture && logoTexture && (
+          <Decal 
+            position={[0, 0.04, 0.15]}
+            rotation={[0, 0, 0]}
+            scale={0.15}
+            map={logoTexture}
+            anisotropy={16} // Fixed anisotropy issue
+            depthTest={false}
+            depthWrite={true}
+          />
+        )}
+      </mesh>
+    </group>
+  );
+};
+
+export default Shirt;
